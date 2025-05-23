6cea0686c0f20505235678e1e29cb46213d022d6
Eduard <edwardamirov@gmail.com>
2025-05-23 12:22:40 +0500

    1

---
+++ replica.sql
@@ -0,0 +1,6 @@
+ALTER TABLE discounts_app_city REPLICA IDENTITY FULL;
+ALTER TABLE discounts_app_company REPLICA IDENTITY FULL;
+ALTER TABLE discounts_app_discountcard REPLICA IDENTITY FULL;
+ALTER TABLE discounts_app_discountcategory REPLICA IDENTITY FULL;
+ALTER TABLE discounts_app_discountcard_categories REPLICA IDENTITY FULL;
+ALTER TABLE discounts_app_discountcard_cities REPLICA IDENTITY FULL;
